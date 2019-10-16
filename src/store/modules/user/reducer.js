f6f3ee13727872806d85bd3942e05c007f51aafe
Lucas Mondini <lcmondini@gmail.com>
2019-10-16 01:28:16 -0300

    Appointments Routes

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -12,7 +12,6 @@ export default function user(state = INITIAL_STATE, action) {
         break;
       }
       case '@user/UPDATE_PROFILE_SUCCESS': {
-        console.tron.warn(action.payload.profile);
         draft.profile = action.payload.profile;
         break;
       }
