b6eaa22f2fdd7d40bd2a11fa552b807486b490c3
Lucas Mondini <lcmondini@gmail.com>
2019-10-16 00:30:33 -0300

    Profile Style

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -12,6 +12,7 @@ export default function user(state = INITIAL_STATE, action) {
         break;
       }
       case '@user/UPDATE_PROFILE_SUCCESS': {
+        console.tron.warn(action.payload.profile);
         draft.profile = action.payload.profile;
         break;
       }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,19 +7,16 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
-
     const response = yield call(api.put, 'users', profile);
 
     Alert.alert('Sucesso!', 'Perfil atualizado com sucesso');
-    toast.success('Perfil atualizado com sucesso!');
 
     yield put(updateProfileSuccess(response.data));
   } catch (err) {
